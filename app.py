1452611fc7f49d7ca93612f49e564e5a17cebcd9
Markwatain1 <kristinamishutina.filth@gmail.com>
2019-09-09 06:50:45 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,13 @@
+def app(environ, start_response):
+            start_response('200 OK', [('Content-Type', 'text/html')])
+                        return('''<html>
+                                        <head>
+                                                                 <title>Hello World!</title>
+                                                                                              </head>
+                                                                                                                               <body>
+                                                                                                                                                                         <h1>Hello world!</h1>
+                                                                                                                                                                                                                       </body>
+                                                                                                                                                                                                                                                                     </html>''')
+
+                                    if __name__ == "__main__":
+                                                            WSGIServer(app, bindAddress = '/tmp/fcgi.sock').run()
